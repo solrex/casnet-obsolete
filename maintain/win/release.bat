dffe9bd5716d5a746c361143400bd0fef65c2186
solrex <solrex@1a74319c-b84c-0410-a804-51ced743ce64>
2009-02-10 18:06:03 +0000

    Updated windows maintain script.

--- maintain/win/release.bat
+++ maintain/win/release.bat
@@ -1,2 +1,3 @@
+xcopy /YS ..\..\src\* .
 python setup_4_py2exe.py py2exe
 makensis.exe casnet.nsi
--- maintain/win/setup_4_py2exe.py
+++ maintain/win/setup_4_py2exe.py
@@ -4,19 +4,19 @@ import py2exe
 setup(
   name = 'casnet-gui',
   description = 'CASNET',
-  version = '1.2',
+  version = '1.3',
 
   windows = [
               {
                 'script': 'casnet-gui.py',
-                'icon_resources': [(1, "pics/casnet.ico")],
+                'icon_resources': [(1, 'pics/casnet.ico')],
               }
             ],
 
   options = {
               'py2exe': {
-                'packages':'encodings',
-                'includes': 'cairo, pango, pangocairo, atk, gobject',
+                'packages': 'encodings',
+                'includes': 'cairo, pango, atk, pangocairo, gobject',
               }
             },
 
