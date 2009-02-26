8b034d582a1b838fd2c70fc5d171e42f51d329c1
solrex <solrex@1a74319c-b84c-0410-a804-51ced743ce64>
2009-02-26 11:37:42 +0000

    Updated rpm build scripts.

--- trunk/maintain/redhat/release.sh
+++ trunk/maintain/redhat/release.sh
@@ -4,11 +4,11 @@ mkdir -p casnet-$VERSION
 cp -r ../../src casnet-$VERSION
 cp -r ../../misc casnet-$VERSION
 cp ../../Makefile casnet-$VERSION
-tar -czvf casnet.tar.gz casnet-$VERSION/ > /dev/null
-mv casnet.tar.gz /usr/src/redhat/SOURCES
+tar -czvf casnet-$VERSION.tar.gz casnet-$VERSION/ > /dev/null
+mv casnet-$VERSION.tar.gz /usr/src/redhat/SOURCES
 cp casnet.spec /usr/src/redhat/SPECS
 cd /usr/src/redhat/SPECS
 rpmbuild -ba casnet.spec
 cd -
 cp /usr/src/redhat/RPMS/i386/casnet*.rpm .
-rm -rf casnet-$VERSION casnet.tar.gz
+rm -rf casnet-$VERSION casnet-$VERSION.tar.gz
