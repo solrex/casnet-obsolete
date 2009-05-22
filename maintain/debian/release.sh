2d0089429f56b00f8fe45ad7f21768082562c64d
solrex <solrex@1a74319c-b84c-0410-a804-51ced743ce64>
2009-05-22 16:07:59 +0000

    Update maintain scripts.

--- maintain/debian/release.sh
+++ maintain/debian/release.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
+VERSION=`../version`
+sed -i -e "s/^Version:.*/Version: $VERSION/g" control
 mkdir -p fakeroot/DEBIAN
 cp control postinst prerm postrm fakeroot/DEBIAN
 cd ../../
 make -e PREFIX=maintain/debian/fakeroot/usr/ -e INS_FLAGS="-o root -g root" install 
 cd maintain/debian/fakeroot
-cp ../casnet.desktop usr/share/applications/casnet.desktop
+sed -i -e "s@^Icon=.*@Icon=/usr/share/icons/casnet.png@g" usr/share/applications/casnet.desktop
 find usr/ -type f -exec md5sum {} + > DEBIAN/md5sums
 cd ../
-dpkg -b fakeroot/ casnet.deb
+dpkg -b fakeroot/ ../casnet-$VERSION-all.deb
 rm -rf fakeroot
